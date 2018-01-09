2328fc6294b047f76d2ef3d0cdeac6c9ed34b712
feigebaobei <18515195415@163.com>
2018-01-09 23:54:13 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import VueDraggable from 'vue-draggable'
+
+Vue.use(VueDraggable)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
