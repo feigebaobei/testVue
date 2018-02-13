928ebcf455d5f69c38e34ccdb50ccd5a0b3275f6
feigebaobei <18515195415@163.com>
2018-02-13 16:25:52 +0800

    frontEndSplit

---
+++ src/components/frontEndSplit/front.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+  default export {
+    data() {
+      return {}
+    },
+    methods: {},
+    computed: {}
+  }
+</script>
+
+<style>
+  
+</style>
---
+++ src/vuex/frontEndSplit.js
@@ -0,0 +1,1 @@
+frontEndSplit.js
